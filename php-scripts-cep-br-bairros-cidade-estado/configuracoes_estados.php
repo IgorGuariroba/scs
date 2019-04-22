83e58ad096503ed59c0e03d76a67e8d66f177c51
Atmos Maciel <atmos.mps@gmail.com>
2019-04-22 17:12:45 -0300

    Adicionando Scripts de enderecos e dados relativos a CEP, cidades, Estados, Enderecos.

---
+++ php-scripts-cep-br-bairros-cidade-estado/configuracoes_estados.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+$configuracoes_estados = array(
+  array('idEstado' => '1','idPais' => '1','dsUF' => 'AC','nmEstado' => 'Acre','nrIBGE' => '12','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '2','idPais' => '1','dsUF' => 'AL','nmEstado' => 'Alagoas','nrIBGE' => '27','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '3','idPais' => '1','dsUF' => 'AM','nmEstado' => 'Amazonas','nrIBGE' => '13','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '4','idPais' => '1','dsUF' => 'AP','nmEstado' => 'Amapá','nrIBGE' => '16','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '5','idPais' => '1','dsUF' => 'BA','nmEstado' => 'Bahia','nrIBGE' => '29','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '6','idPais' => '1','dsUF' => 'CE','nmEstado' => 'Ceará','nrIBGE' => '23','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '7','idPais' => '1','dsUF' => 'DF','nmEstado' => 'Diistrito Federal','nrIBGE' => '53','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '8','idPais' => '1','dsUF' => 'ES','nmEstado' => 'Espírito Santo','nrIBGE' => '32','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '9','idPais' => '1','dsUF' => 'GO','nmEstado' => 'Goiás','nrIBGE' => '52','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '10','idPais' => '1','dsUF' => 'MA','nmEstado' => 'Maranhão','nrIBGE' => '21','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '11','idPais' => '1','dsUF' => 'MG','nmEstado' => 'Minas Gerais','nrIBGE' => '31','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '12','idPais' => '1','dsUF' => 'MS','nmEstado' => 'Mato Groso do Sul','nrIBGE' => '50','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '13','idPais' => '1','dsUF' => 'MT','nmEstado' => 'Mato Grosso','nrIBGE' => '51','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '14','idPais' => '1','dsUF' => 'PA','nmEstado' => 'Pará','nrIBGE' => '15','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '15','idPais' => '1','dsUF' => 'PB','nmEstado' => 'Paraíba','nrIBGE' => '25','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '16','idPais' => '1','dsUF' => 'PE','nmEstado' => 'Pernambuco','nrIBGE' => '26','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '17','idPais' => '1','dsUF' => 'PI','nmEstado' => 'Piauí','nrIBGE' => '22','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '18','idPais' => '1','dsUF' => 'PR','nmEstado' => 'Paraná','nrIBGE' => '41','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '19','idPais' => '1','dsUF' => 'RJ','nmEstado' => 'Rio de Janeiro','nrIBGE' => '33','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '20','idPais' => '1','dsUF' => 'RN','nmEstado' => 'Rio Grande do Norte','nrIBGE' => '24','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '21','idPais' => '1','dsUF' => 'RO','nmEstado' => 'Rondônia','nrIBGE' => '11','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '22','idPais' => '1','dsUF' => 'RR','nmEstado' => 'Roraima','nrIBGE' => '14','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '23','idPais' => '1','dsUF' => 'RS','nmEstado' => 'Rio Grande do Sul','nrIBGE' => '43','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '24','idPais' => '1','dsUF' => 'SC','nmEstado' => 'Santa Catarina','nrIBGE' => '42','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '25','idPais' => '1','dsUF' => 'SE','nmEstado' => 'Sergipe','nrIBGE' => '28','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '26','idPais' => '1','dsUF' => 'SP','nmEstado' => 'São Paulo','nrIBGE' => '35','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '27','idPais' => '1','dsUF' => 'TO','nmEstado' => 'Tocantis','nrIBGE' => '17','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Sim','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '30','idPais' => '1','dsUF' => 'te','nmEstado' => 'teste','nrIBGE' => '0','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Não','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '31','idPais' => '1','dsUF' => 'NM','nmEstado' => 'MATAGAL','nrIBGE' => '3232','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Não','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL),
+  array('idEstado' => '32','idPais' => '1','dsUF' => 'TE','nmEstado' => 'teste 22','nrIBGE' => '222','dsLatitude' => '','dsLongitude' => '','isAtivo' => 'Não','idPessoaAutor' => '1','dtCriacao' => '2015-12-18 10:54:08','dtAtualizacao' => NULL)
+);
